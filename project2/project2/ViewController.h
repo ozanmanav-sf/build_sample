a9e91ffe8460684f6288450178e4e1eb25a9777b
doganekici <doganekici26@gmail.com>
2019-04-04 14:36:28 +0300

    add scheme

---
+++ project2/project2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  project2
+//
+//  Created by Dogan Ekici on 4.04.2019.
+//  Copyright © 2019 Dogan Ekici. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ project2/project2Tests/project2Tests.m
@@ -0,0 +1,37 @@
+//
+//  project2Tests.m
+//  project2Tests
+//
+//  Created by Dogan Ekici on 4.04.2019.
+//  Copyright © 2019 Dogan Ekici. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface project2Tests : XCTestCase
+
+@end
+
+@implementation project2Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
