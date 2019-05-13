b8aae3a6fb77a737326dc7bf48fd242ba75d6199
Dogan Ekici <doganekici26@gmail.com>
2019-05-13 15:21:17 +0300

    Added test projects

---
+++ work1/work2/work3/denemeproj4/denemeproj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  denemeproj
+//
+//  Created by Dogan Ekici on 13.05.2019.
+//  Copyright © 2019 Dogan Ekici. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
